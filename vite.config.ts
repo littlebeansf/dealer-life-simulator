23eff3ccf594034c3c08c75412d42f376ddaa352
Sebastian Fries <sebastianfries.business@gmail.com>
2025-04-20 12:17:00 +0200

    First state working with gh-pages.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/dealer-life-simulator/", // << your GitHub repo name!
+  base: "/dealer-life-simulator/", // ✅ VERY IMPORTANT
   plugins: [react()],
 });
